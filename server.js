adb5e86bfbd382eaf01cdcb14bb1640082c39e2b
Tufail Ali <alitufail9569@gmail.com>
2022-11-03 01:20:10 +0500

    After deploy2

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-const mongoose = require('mongoose');
-const dotenv = require('dotenv');
-dotenv.config({ path: './config.env' });
-const app = require('./app');
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
 
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT;
 const DB = process.env.DATABASE.replace(
-  '<PASSWORD>',
+  "<PASSWORD>",
   process.env.MONGODB_PASSWORD
 );
 
@@ -16,7 +16,7 @@ mongoose
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('DATABSE CONNETED'));
+  .then(() => console.log("DATABSE CONNETED"));
 
 app.listen(port, () => {
   console.log(`App running at port ${port}....`);
